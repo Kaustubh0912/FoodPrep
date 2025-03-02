d51fbba0856ae876b942d78f87a61812f1645b40
Kaustubh <108586950+Kaustubh0912@users.noreply.github.com>
2025-03-02 17:23:51 +0530

    added food item card css

--- src/components/FoodItemCard/FoodItemCard.jsx
+++ src/components/FoodItemCard/FoodItemCard.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import { assets } from '../../assets/assets'
 import './FoodItemCard.css'
-const FoodItemCard = ({id,name,price,description,image}) => {
+const FoodItemCard = ({ id, name, price, description, image }) => {
   return (
     <div className='food-item'>
       <div className="food-item-image-container">
         <img className='food-item-image' src={image} alt={name} />
       </div>
-      <p className='food-item-name'>{name}</p>
-      <p className='food-item-description'>{description}</p>
-      <div className="price-rating">
-        <p className='food-item-price'> ₹{price}</p>
-        <img src={assets.rating_starts} alt="" />
+      <div className="food-info">
+        <p className='food-item-name'>{name}</p>
+        <p className='food-item-description'>{description}</p>
+        <div className="price-rating">
+          <p className='food-item-price'> ₹{price}</p>
+          <img src={assets.rating_starts} alt="" />
+        </div>
       </div>
     </div>
   )
