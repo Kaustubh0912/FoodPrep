010a2e41f8a6e7a49e78bd8abd0bc2db6d67b9e6
Kaustubh <108586950+Kaustubh0912@users.noreply.github.com>
2025-03-02 18:00:05 +0530

    updated useState in order to track individual items

--- src/components/FoodItemCard/FoodItemCard.jsx
+++ src/components/FoodItemCard/FoodItemCard.jsx
@@ -1,22 +1,38 @@
-import React from 'react'
-import { assets } from '../../assets/assets'
-import './FoodItemCard.css'
-const FoodItemCard = ({ id, name, price, description, image }) => {
-  return (
-    <div className='food-item'>
-      <div className="food-item-image-container">
-        <img className='food-item-image' src={image} alt={name} />
-      </div>
-      <div className="food-info">
-        <p className='food-item-name'>{name}</p>
-        <p className='food-item-description'>{description}</p>
-        <div className="price-rating">
-          <p className='food-item-price'> ₹{price}</p>
-          <img src={assets.rating_starts} alt="" />
-        </div>
-      </div>
-    </div>
-  )
-}
+import { useContext } from 'react';
+import { assets } from '../../assets/assets';
+import { StoreContext } from '../../context/StoreContext';
+import './FoodItemCard.css';
 
-export default FoodItemCard
+const FoodItemCard = ({ id, name, price, description, image }) => 
+{
+    const { cartItems,addToCart, removeFromCart } = useContext(StoreContext);
+    const itemCount = cartItems[id] || 0;
+
+    return (
+        <div className='food-item'>
+            <div className="food-item-image-container">
+                <img className='food-item-image' src={image} alt={name} />
+                {
+                    itemCount === 0 ?
+                    <img className="add" src={assets.add_icon_white} alt="Add" onClick={() => addToCart(id)} />
+                    :
+                    <div className="food-item-counter">
+                        <img src={assets.remove_icon_red} alt="Remove" onClick={() => removeFromCart(id)} />
+                        <span>{itemCount}</span>
+                        <img src={assets.add_icon_green} alt="Add" onClick={() => addToCart(id)} />
+                    </div>
+                }
+            </div>
+            <div className="food-info">
+                <p className='food-item-name'>{name}</p>
+                <p className='food-item-description'>{description}</p>
+                <div className="price-rating">
+                    <p className='food-item-price'> ₹{price}</p>
+                    <img src={assets.rating_starts} alt="Rating" />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default FoodItemCard;
--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -1,13 +1,36 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { food_list } from '../assets/assets'
 
 
 export const StoreContext = createContext(null)
 
 const StoreContextProvider = (props) => {
-    const contextValue = {
-        food_list
+    const [cartItems,setCartItems] = useState({})
+    
+
+    const addToCart=(itemId) => {
+        if(!cartItems[itemId]){
+            setCartItems({...cartItems, [itemId]:1})
+        }
+        else{
+            setCartItems({...cartItems, [itemId]:cartItems[itemId]+1})
+        }
     }
+    
+    const removeFromCart=(itemId) => {
+        if(cartItems[itemId]>0){
+            setCartItems({...cartItems, [itemId]:cartItems[itemId]-1})
+        }
+    }
+
+    const contextValue = {
+        food_list,
+        cartItems,
+        setCartItems,
+        addToCart,
+        removeFromCart,
+    }
+    
     return(
         <StoreContext.Provider value={contextValue}>
             {props.children}
