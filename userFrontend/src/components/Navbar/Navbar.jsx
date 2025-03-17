17b6d3595591f902c7bc1577155d9207817da2f6
Kaustubh <108586950+Kaustubh0912@users.noreply.github.com>
2025-03-17 20:50:56 +0530

    add total logic and dynamic class for the cart symbol

--- userFrontend/src/components/Navbar/Navbar.jsx
+++ userFrontend/src/components/Navbar/Navbar.jsx
@@ -1,26 +1,27 @@
-import {React,useState} from 'react'
-import { assets } from '../../assets/assets'
+import {useState,useContext} from 'react'
 import {Link} from 'react-router-dom'
-import "./Navbar.css"
+import {assets} from '../../assets/assets'
+import {StoreContext} from '../../context/StoreContext'
+import './Navbar.css'
 
 const Navbar = ({setShowLogin}) => {
-  const [menu, setMenu] = useState("home")
+  const [menu,setMenu] = useState("home")
+  const {getTotalCartAmount} = useContext(StoreContext);
   return (
     <div className='navbar'>
-        <Link to='/'><img className='logo' src={assets.logo} alt='Logo' /></Link>
-        <ul className='navbar-menu'>
-             <Link to='/' onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>Home</Link>
-             <a href="#explore-menu"><li onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>Menu</li></a>
-             <a href="#footer"><li onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>Contact us</li></a>
+        <Link to='/' > <img className='logo' src={assets.logo} alt="" /></Link>
+        <ul className="navbar-menu">
+            <Link to='/' onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>Home</Link>
+            <a href="#explore-menu"><li onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>Menu</li></a>
+            <a href="#footer"><li onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>Contact us</li></a>
         </ul>
         <div className="navbar-right">
             <div className="basket-dot">
-                <Link to="/cart"><img src={assets.basket_icon} alt="cart" /></Link>
-                <div className="dot"></div>
+                <Link to="/cart"><img src={assets.basket_icon} alt="" /></Link>
+                <div className={getTotalCartAmount()===0?"":"dot"}></div>
             </div>
             <button onClick={()=>setShowLogin(true)}>Sign Up</button>
         </div>
-      
     </div>
   )
 }
--- userFrontend/src/context/StoreContext.jsx
+++ userFrontend/src/context/StoreContext.jsx
@@ -1,41 +1,49 @@
-import { createContext, useState } from "react";
-import { food_list } from '../assets/assets'
-
+import {createContext,useState} from 'react'
+import {food_list} from '../assets/assets'
 
 export const StoreContext = createContext(null)
 
-const StoreContextProvider = (props) => {
+const StoreContexProvider = (props)=>{
     const [cartItems,setCartItems] = useState({})
-    
 
-    const addToCart=(itemId) => {
+    const addToCart = (itemId)=>{
         if(!cartItems[itemId]){
-            setCartItems({...cartItems, [itemId]:1})
+            setCartItems({...cartItems,[itemId]:1})
         }
         else{
-            setCartItems({...cartItems, [itemId]:cartItems[itemId]+1})
-        }
-    }
-    
-    const removeFromCart=(itemId) => {
-        if(cartItems[itemId]>0){
-            setCartItems({...cartItems, [itemId]:cartItems[itemId]-1})
+            setCartItems({...cartItems,[itemId]:cartItems[itemId]+1})
         }
     }
 
-    const contextValue = {
+    const removeFromCart = (itemId)=>{
+        setCartItems({...cartItems,[itemId]:cartItems[itemId]-1})
+    }
+
+    const getTotalCartAmount=()=>{
+        let total=0;
+        for(let item in cartItems){
+            if(cartItems[item]>0){
+                let itemInfo = food_list.find(food=>food._id===item)
+                total+=itemInfo.price*cartItems[item];
+            }
+        }
+        return total;
+    }
+
+    const contextValue={
+        getTotalCartAmount,
         food_list,
         cartItems,
         setCartItems,
         addToCart,
-        removeFromCart,
+        removeFromCart        
     }
-    
-    return(
+
+    return (
         <StoreContext.Provider value={contextValue}>
             {props.children}
         </StoreContext.Provider>
     )
 }
 
-export default StoreContextProvider;
+export default StoreContexProvider;
