39af3308b600c4f4d5d0a66139fcd6a6abe59764
Kaustubh <108586950+Kaustubh0912@users.noreply.github.com>
2025-03-20 20:27:33 +0530

    add basic form structure for adding item

--- adminFrontend/src/screens/Add/Add.jsx
+++ adminFrontend/src/screens/Add/Add.jsx
@@ -1,9 +1,48 @@
 import React from 'react'
 import './Add.css'
+import {assets} from '../../assets/assets'
 const Add = () => {
   return (
-    <div>
-      Add
+    <div className='screen'>
+      <div className="container">
+        <form >
+          <div className="add-img-upload">
+            <p>Upload Image</p>
+            <label htmlFor="image">
+              <img src={assets.upload_area} alt="" />
+            </label>
+            <input type="file" id="image" hidden required />
+          </div>
+          <div className="add-product-name">
+            <p>Name</p>
+            <input type="text" placeholder='Type here' />
+          </div>
+          <div className="add-product-name">
+            <p>Name</p>
+            <textarea rows='6' placeholder='Write content here' ></textarea>
+          </div>
+          <div className="add-category-price">
+            <div className="add-category">
+              <p>Category</p>
+              <select name="category">
+                <option value="Salad">Salad</option>
+                <option value="Rolls">Rolls</option>
+                <option value="Deserts">Deserts</option>
+                <option value="Sandwich">Sandwich</option>
+                <option value="Cake">Cake</option>
+                <option value="Pure Veg">Pure Veg</option>
+                <option value="Pasta">Pasta</option>
+                <option value="Noodles">Noodles</option>
+              </select>
+            </div>
+            <div className="add-price">
+              <p>Price</p>
+              <input type="number" placeholder='₹0' />
+            </div>
+          </div>
+          <button type="submit"className="add-btn">Add Product</button>
+        </form>
+      </div>
     </div>
   )
 }
