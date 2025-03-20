536a65705f3395069ae632178f4dd3317ad637c9
Kaustubh <108586950+Kaustubh0912@users.noreply.github.com>
2025-03-20 21:04:27 +0530

    add form styles to the page

--- adminFrontend/src/screens/Add/Add.jsx
+++ adminFrontend/src/screens/Add/Add.jsx
@@ -1,33 +1,34 @@
 import React from 'react'
 import './Add.css'
 import {assets} from '../../assets/assets'
+
 const Add = () => {
   return (
     <div className='screen'>
       <div className="container">
-        <form >
-          <div className="add-img-upload">
+        <form className='flex-col'>
+          <div className="add-img-upload flex-col">
             <p>Upload Image</p>
             <label htmlFor="image">
               <img src={assets.upload_area} alt="" />
             </label>
-            <input type="file" id="image" hidden required />
+            <input type="file" id='image' hidden required />
           </div>
-          <div className="add-product-name">
+          <div className="add-product-name flex-col">
             <p>Name</p>
             <input type="text" placeholder='Type here' />
           </div>
-          <div className="add-product-name">
-            <p>Name</p>
-            <textarea rows='6' placeholder='Write content here' ></textarea>
+          <div className="add-product-desc flex-col">
+            <p>Description</p>
+            <textarea rows='6' placeholder='Write content here'></textarea>
           </div>
           <div className="add-category-price">
-            <div className="add-category">
+            <div className="add-category flex-col">
               <p>Category</p>
-              <select name="category">
+              <select  >
                 <option value="Salad">Salad</option>
                 <option value="Rolls">Rolls</option>
-                <option value="Deserts">Deserts</option>
+                <option value="Dessert">Dessert</option>
                 <option value="Sandwich">Sandwich</option>
                 <option value="Cake">Cake</option>
                 <option value="Pure Veg">Pure Veg</option>
@@ -35,12 +36,12 @@ const Add = () => {
                 <option value="Noodles">Noodles</option>
               </select>
             </div>
-            <div className="add-price">
+            <div className="add-price flex-col">
               <p>Price</p>
-              <input type="number" placeholder='₹0' />
+              <input type="Number" placeholder='₹150'/>
             </div>
           </div>
-          <button type="submit"className="add-btn">Add Product</button>
+          <button type='submit' className='add-btn'>Add</button>
         </form>
       </div>
     </div>
