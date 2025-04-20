d4b494c16381653c0c1dba5ca54090ec256496f6
Kaustubh <108586950+Kaustubh0912@users.noreply.github.com>
2025-04-20 14:23:11 +0530

    completed once again

--- userFrontend/src/components/Footer/Footer.jsx
+++ userFrontend/src/components/Footer/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
       <div className="footer-content">
         <div className="footer-content-left">
           <img className='footer-logo' src={assets.logo_bottom} alt="" />
-          <p>Food Prep is a full-stack project designed for hands-on teaching, helping students learn full-stack development. It's used by FACEPrep, an ed-tech company focused on equipping students with the skills to achieve their career aspirations.</p>
+          <p>FoodPrep is a sleek, full-stack food delivery platform that brings restaurant-quality meals directly to your doorstep. With an intuitive interface and seamless ordering process, FoodPrep makes finding and enjoying your favorite dishes effortless.</p>
           <div className="footer-social-icons">
             <img src={assets.facebook_icon} alt="" />
             <img src={assets.twitter_icon} alt="" />
@@ -33,7 +33,7 @@ const Footer = () => {
         </div>
       </div>
       <hr />
-      <p className="footer-copyright">Copyright 2024 © 2021 FoodPrep. All rights reserved.</p>
+      <p className="footer-copyright">Copyright 2025 © 2021 FoodPrep. All rights reserved.</p>
     </div>
   )
 }
